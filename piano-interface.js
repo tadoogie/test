bebba1ee277cb9b35f6079ffc6d005df60019868
tadoogie <tim.duguid@glasgow.ac.uk>
2026-01-09 04:51:32 +0000

    Refactor pitch class mapping to start from 0
    Updated pitch class mapping to start from 0 instead of 1.

--- piano-interface.js
+++ piano-interface.js
@@ -5,10 +5,10 @@ console.log('Piano interface script file loaded!    ');
   console.log('Piano interface IIFE executing');
   
   // Pitch class mapping (C=1)
-  const pitchClassMap = {
-    'C': '1', 'C#': '2', 'D': '3', 'D#': '4', 'E': '5', 'F': '6',
-    'F#': '7', 'G': '8', 'G#': '9', 'A': '10', 'A#': '11', 'B': '12'
-  };
+const pitchClassMap = {
+  'C': '0', 'C#': '1', 'D': '2', 'D#': '3', 'E':  '4', 'F': '5',
+  'F#':  '6', 'G': '7', 'G#': '8', 'A': '9', 'A#': '10', 'B': '11'
+};
   
   // Sol-fa mapping (fixed Do = C)
   const solfaMap = {
@@ -408,3 +408,4 @@ console.log('Piano interface script file loaded!    ');
 })();
 
 console.log('Piano interface loaded');
+
